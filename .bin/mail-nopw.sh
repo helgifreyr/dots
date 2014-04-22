f55b32d675d3c7fd82e7afac8d786c401f0c696f
helgi <helgi.runarsson@gmail.com>
2014-04-22 11:49:20 +0100

    new file:   .bin/mail-nopw.sh 	renamed:    .xmobarrc-nopw -> .xmobarrc

--- .bin/mail-nopw.sh
+++ .bin/mail-nopw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
-gmail_login=helgi.runarsson
-gmail_password=8piG/c^4g
+gmail_login=user
+gmail_password=password
  
 dane=`curl -su ${gmail_login}:${gmail_password} https://mail.google.com/mail/feed/atom | grep 'fullcount' | sed -r 's|.*>(.*)</.*|\1|'`
 
